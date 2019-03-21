2c54152eb74a6de8ed6f80eaedadba00e6dc558c
RCmerci <rcmerci@gmail.com>
2019-03-21 19:20:17 +0800

    update readme

--- goe-movement.el
+++ goe-movement.el
@@ -2,6 +2,8 @@
 
 ;; Copyright (C) 2019  rcmerci
 
+;; Author: rcmerci <rcmerci@gmail.com>
+
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
